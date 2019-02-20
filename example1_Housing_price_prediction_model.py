628855e798d511cb0b59891a9a9eaf1e5d58fe91
jinyuaa <596338260@qq.com>
2019-02-20 15:19:03 +0800

    用 tensorflow 实现房价预测模型 多元线性：包括数据读入、规范化、处理（添ones列、获取x和y）、创建数据流图、创建会话，数据训练得出拟合函数 y = w0 + w1x1 + w2x2

---
+++ example1_Housing_price_prediction_model.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import seaborn as sns
+from mpl_toolkits import mplot3d
+
+'''
+# 单变量房价预测模型
+sns.set(context="notebook", style="whitegrid", palette="dark")
+
+df0 = pd.read_csv('data0.csv', names=['square', 'price'])
+sns.lmplot('square', 'price', df0, height=6, fit_reg=True)
+plt.show()
+'''
+
+# 多变量房价预测模型
+df1 = pd.read_csv('data1.csv', names=['square', 'bedrooms', 'price'])
+
+
+# 数据规范化
+def normalize_feature(df):
+    return df.apply(lambda column: (column - column.mean()) / column.std())
+
+
+df1 = normalize_feature(df1)
+'''
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.set_xlabel('square')
+ax.set_ylabel('bedrooms')
+ax.set_zlabel('price')
+ax.scatter3D(df1['square'], df1['bedrooms'], df1['price'], c=df1['price'], cmap='Greens')
+# plt.show()
+'''
+# 数据处理：添加 ones 列
+ones = pd.DataFrame({'ones': np.ones(len(df1))})
+df = pd.concat([ones, df1], axis=1)
+# print(df.head())
+
+# 数据处理：获取 x 和 y
+X_data = np.array(df[df.columns[0:3]])
+y_data = np.array(df[df.columns[-1]]).reshape(len(df), 1)
+
+print(y_data.shape)
+# 创建线性回归模型(数据流图)
+# 定义参数
+alpha = 0.01   # 学习率
+epoch = 500    # 训练全量数据的轮数
+
+# 创建线性回归模型(即数据流图)
+# 输入 X 的形状为 [47,3]
+X = tf.placeholder(tf.float32, X_data.shape)
+# 输出 y 的形状为 [47,1]
+y = tf.placeholder(tf.float32, y_data.shape)
+
+# 权重变量 W 形状 [3,1]
+W = tf.get_variable("weights", (X_data.shape[1], 1), initializer=tf.constant_initializer())
+# 假设函数 h(x)=w0*x0+w1*x1+w2*x2 其中x0恒为1
+# 推理值 y_pred 形状为[47,1]
+y_pred = tf.matmul(X, W)
+
+# 损失函数采用最小二乘法 (y_pred - y) 是形如[47,1]的向量
+# tf.matmul(a, b, transpose_a=True) 表示矩阵a的转置乘矩阵b,即[1,47]x[47,1]
+# 实现了 作差 求和 乘以 1/2n,即损失函数
+loss_op = 1 / (2 * len(X_data)) * tf.matmul((y_pred-y), (y_pred-y), transpose_a=True)
+# 随机梯度下降优化器
+opt = tf.train.GradientDescentOptimizer(learning_rate=alpha)
+# 单轮训练操作
+train_op = opt.minimize(loss_op)
+
+# 创建会话(运行环境)
+with tf.Session() as sess:
+    # 初始化全局变量
+    sess.run(tf.global_variables_initializer())
+
+    # 开始训练模型
+    # 因为训练集较少，所以采用梯度下降优化算法，每次都使用全量数据训练
+    for step in range(1, epoch+1):
+        sess.run([train_op], feed_dict={X: X_data, y: y_data})
+        if step % 10 == 0:
+            loss, w = sess.run([loss_op, W], feed_dict={X: X_data, y: y_data})
+            log_str = "Epoch %d \t Loss = %.4g \t Model: y = %.4gx1 + %.4gx2 + %.4g"
+            print(log_str % (step, loss, w[1], w[2], w[0]))
+
+
+# Epoch 500  Loss = 0.132  Model: y = 0.8304x1 + 0.0008239x2 + 4.138e-09
+
